e067f886819be4c4a312ba6551649a8cfb542293
Lex <lex.hrabovskyi@gmail.com>
2020-09-01 20:07:33 +0300

    Initial Commit

---
+++ Battleship AR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Battleship AR
+//
+//  Created by Александр on 01.09.2020.
+//  Copyright © 2020 Александр. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
