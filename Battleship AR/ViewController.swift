938850b5d15b54462d63995330e0ee8d0705b7eb
Oleksandr <38527824+LexHrabovskyi@users.noreply.github.com>
2020-09-02 20:03:48 +0300

    Feat: added basic enemy field (#1)

--- Battleship AR/ViewController.swift
+++ Battleship AR/ViewController.swift
@@ -13,13 +13,52 @@ class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    private let enemyFieldOffset: Float = 0.15
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let mainAnchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.4, 0.6])
+        arView.scene.addAnchor(mainAnchor)
+        addTheSea(toAnchor: mainAnchor)
+        addSeaFields(toAnchor: mainAnchor)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
     }
+    
+    private func addTheSea(toAnchor anchor: AnchorEntity) {
+        
+        let seaMesh = MeshResource.generateBox(width: 0.8, height: 0.01, depth: 0.5)
+        let seaMetarial = SimpleMaterial(color: .blue, roughness: .float(0.05), isMetallic: false)
+        let model = ModelEntity(mesh: seaMesh, materials: [seaMetarial])
+        model.position = [0, -0.01, 0]
+        anchor.addChild(model)
+        
+    }
+    
+    private func addSeaFields(toAnchor anchor: AnchorEntity) {
+        
+        var seaFields = [Entity]()
+        for _ in 1...100 {
+            
+            let fieldMesh = MeshResource.generateBox(width: 0.02, height: 0.005, depth: 0.02)
+            let waterMaterial = SimpleMaterial(color: .cyan, roughness: .float(0.2), isMetallic: false)
+            let fieldModel = ModelEntity(mesh: fieldMesh, materials: [waterMaterial])
+            fieldModel.generateCollisionShapes(recursive: true)
+            seaFields.append(fieldModel)
+            
+        }
+        
+        for (index, field) in seaFields.enumerated() {
+            
+            let x = Float(index % 10)
+            let z = Float(index / 10)
+            field.position = [enemyFieldOffset + x * 0.022, 0, z * 0.022]
+            anchor.addChild(field)
+            
+        }
+        
+    }
+    
+    
+    
 }
